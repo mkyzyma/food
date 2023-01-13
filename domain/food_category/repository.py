d76153552154eabf9bce0245bea858bb26e37939
Mikhail Kyzyma <mkyzyma@gmail.com>
2023-01-13 09:26:31 +0500

    Cleanup code

--- domain/food_category/repository.py
+++ domain/food_category/repository.py
@@ -33,34 +33,4 @@ class FoodCategoryRepository(Repository[FoodCategory]):
         if hasattr(filter, "toppings") and filter.toppings != None:
             stmt = stmt.where(Topping.name.in_(filter.toppings))
 
-        # stmt.options(contains_eager(FoodCategory.foods))
-        # if hasattr(filter, "is_publish"):
-        #     stmt = stmt.options(
-        #         joinedload(
-        #             FoodCategory.foods.and_(Food.is_publish == filter.is_publish)
-        #         )
-        #     ).where(FoodCategory.is_publish == filter.is_publish)
-
-        # if hasattr(filter, "is_vegan") and filter.is_vegan != None:
-        #     stmt = (
-        #         stmt.join(FoodCategory.foods)
-        #         .where(Food.is_vegan == filter.is_vegan)
-        #         .options(
-        #             joinedload(
-        #                 FoodCategory.foods.and_(Food.is_vegan == filter.is_vegan)
-        #             )
-        #         )
-        #     )
-
-        # if hasattr(filter, "is_special") and filter.is_special != None:
-        #     stmt = (
-        #         stmt.join(FoodCategory.foods)
-        #         .where(Food.is_special == filter.is_special)
-        #         .options(
-        #             joinedload(
-        #                 FoodCategory.foods.and_(Food.is_special == filter.is_special)
-        #             )
-        #         )
-        #     )
-
         return stmt
